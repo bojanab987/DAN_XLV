ad5a11972d4ac192cb81d4abd9b7a71a82b04b80
bojanabb <bojana.buljic@gmail.com>
2020-07-13 12:25:00 +0200

    LogInView implemented

---
+++ DAN_XLV_Bojana_Buljic/Zadatak_1/View/LogInView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using Zadatak_1.ViewModel;
+
+namespace Zadatak_1.View
+{
+    /// <summary>
+    /// Interaction logic for LogInView.xaml
+    /// </summary>
+    public partial class LogInView : Window
+    {
+        public LogInView()
+        {
+            InitializeComponent();
+            this.DataContext = new LogInViewModel(this);
+        }
+
+        /// <summary>
+        /// Disable login button unil both field are not empty
+        /// </summary>
+        /// <param name="sender">object sender</param>
+        /// <param name="e">RoutedEventArgs event</param>
+        private void txtChanged(object sender, RoutedEventArgs e)
+        {
+
+            if (passwordBox.Password.Length > 0 && txtUsername.Text.Length > 0)
+            {
+                btnLogin.IsEnabled = true;
+            }
+            else
+            {
+                btnLogin.IsEnabled = false;
+            }
+        }
+    }
+}
---
+++ DAN_XLV_Bojana_Buljic/Zadatak_1/ViewModel/LogInViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Zadatak_1.Command;
+using Zadatak_1.View;
+
+namespace Zadatak_1.ViewModel
+{
+    class LogInViewModel:ViewModelBase
+    {
+        LogInView logInView;
+
+        #region Constructor
+        public LogInViewModel(LogInView logInOpen)
+        {
+            logInView = logInOpen;           
+        }
+        #endregion
+
+        #region Properties   
+        private string username;
+        public string Username
+        {
+            get { return username; }
+            set
+            {
+                username = value;
+                OnPropertyChanged("Username");
+            }
+        }
+        #endregion
+
+        #region Commands
+        /// <summary>
+        /// LogIn Command
+        /// </summary>
+        private ICommand login;
+        public ICommand Login
+        {
+            get
+            {
+                if (login == null)
+                {                    
+                    login = new RelayCommand(LoginExecute);
+                }
+                return login;
+            }
+        }
+
+        /// <summary>
+        /// Method for deciding which View will open according to logged in Employee credentials
+        /// </summary>
+        private void LoginExecute(object o)
+        {
+            try
+            {
+                string password = (o as PasswordBox).Password;
+                if (Username == "Mag2019" && password == "Mag2019")
+                {
+                    EmployeeView employee = new EmployeeView();
+                    logInView.Close();
+                    employee.ShowDialog();
+                }
+                else if (Username == "Man2019" && password == "Man2019")
+                {
+                    ManagerView manager = new ManagerView();
+                    logInView.Close();
+                    manager.ShowDialog();
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show("Username or password not correct.");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }       
+        #endregion
+    }
+}
